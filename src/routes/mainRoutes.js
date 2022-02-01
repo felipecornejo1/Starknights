06e097dce5169a9de6ff57cb8c797265004a3d4c
felipecornejo1 <felicornejo@gmail.com>
2022-02-01 20:34:45 -0300

    prueba

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const {body, validationResult, check} = require('express-validator');
 
+router.get('/', mainController.home);
 
+router.get('/proximamente', mainController.proximamente);
 
 module.exports = router;
