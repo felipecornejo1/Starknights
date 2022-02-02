793dc7f7bf6143106494090cb018807880a5aab0
ChristianAdrianAimone <adrian.aimone@gmail.com>
2022-02-02 11:51:44 -0300

    arreglo de un error de escritura el cual no permitia usar bien el codigo, a su vez pequeña actualizacion de crear usuario (sin terminar) y fue creado un generador de Id

--- src/models/User.js
+++ src/models/User.js
@@ -7,28 +7,37 @@ const user = {
         return JSON.parse (FileSystem.readFileSync(this.fileName, "utf-8"));
     },
 
+    generateId: function(){
+        let allUsers = this.findAll();
+        let lastUser = allUsers.pop();
+        if (lastUser){
+            return lastUser.id + 1;
+        }
+        return 1;
+    },
+
     findAll: function(){
         return this.getData();
     },
 
     findByPk: function (id) {
-        let allUsers = thi.findAll();
+        let allUsers = this.findAll();
         let userFound = allUsers.find(oneUser => oneUser.id === id);
         return userFound;
     },
 
     findByField: function (field, text) {
-        let allUsers = thi.findAll();
+        let allUsers = this.findAll();
         let userFound = allUsers.find(oneUser => oneUser[field] === text);
         return userFound;
     },
 
     create: function (userData){
-        let allUsers = thi.findAll();
+        let allUsers = this.findAll();
         allUsers.push(userData);
         fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, " "));
         return true;
     }
 };
 
-console.log(User.getData());
+console.log(User.generateId());
