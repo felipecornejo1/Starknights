0f153b82ac5547a2ed468836eca23f6d166c9ad3
Sbadino <badino.santi@gmail.com>
2022-03-29 18:46:56 -0300

    commit

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', () => {
     let errorPopup = document.querySelector('.error-popup');
     let successPopup = document.querySelector('.success-popup');
+    let btnCarrito = document.querySelector('#btn-carrito');
+    let itemId = document.querySelector('.item-id');
     
     if(errorPopup) {
         errorPopup.classList.add('error-animation');
@@ -10,4 +12,16 @@ window.addEventListener('load', () => {
         successPopup.classList.add('error-animation');
         setTimeout(() => {successPopup.classList.remove('error-animation');}, 3000); 
     }
+
+    btnCarrito.addEventListener('click', function() {
+        //console.log(itemId.innerText);
+        fetch("https://starknights-api.herokuapp.com/items/" + itemId.innerText)
+            .then(res => {
+                return res.json();
+            })
+            .then(item => {
+                localStorage.setItem('carrito' + item.data[0].id, item.data[0].id);
+            })
+    })
 })
+
