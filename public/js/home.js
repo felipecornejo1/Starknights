12cb55f20d2764e5f5047e13b9400ce95560deb5
Sbadino <badino.santi@gmail.com>
2022-04-16 19:34:23 -0300

    JS home

---
+++ public/js/home.js
@@ -0,0 +1,50 @@
+let protossLink = document.querySelector('.protoss-1')
+let terranLink = document.querySelector('.terran-1')
+let zergLink = document.querySelector('.zerg-1')
+
+// PROTOSS
+protossLink.addEventListener('mouseover', () => {
+    console.log("HOVER PROTOSS");
+    protossLink.classList.add('protoss-2');
+});
+
+protossLink.addEventListener('click', () => {
+    window.location.href = "https://stackoverflow.com/questions/4561097/css-box-shadow-bottom-only";
+})
+
+protossLink.addEventListener('mouseout', () => {
+    console.log("HOVER PROTOSS");
+    protossLink.classList.add('protoss-1');
+    protossLink.classList.remove('protoss-2');
+});
+
+// TERRAN
+terranLink.addEventListener('mouseover', () => {
+    console.log("HOVER PROTOSS");
+    terranLink.classList.add('terran-2');
+});
+
+terranLink.addEventListener('click', () => {
+    window.location.href = "https://stackoverflow.com/questions/4561097/css-box-shadow-bottom-only";
+})
+
+terranLink.addEventListener('mouseout', () => {
+    console.log("HOVER PROTOSS");
+    terranLink.classList.add('terran-1');
+    terranLink.classList.remove('terran-2');
+});
+
+// ZERG
+zergLink.addEventListener('mouseover', () => {
+    zergLink.classList.add('zerg-2');
+    
+});
+
+terranLink.addEventListener('click', () => {
+    window.location.href = "https://stackoverflow.com/questions/4561097/css-box-shadow-bottom-only";
+})
+
+zergLink.addEventListener('mouseout', () => {
+    zergLink.classList.add('zerg-1');
+    zergLink.classList.remove('zerg-2');
+});
